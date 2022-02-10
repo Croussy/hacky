65566f1440b9dc45c64387cfe0eb7b3717c993c7
Keclem <keclem8@aol.com>
2022-02-10 10:00:48 +0100

    Change unit test framework to mocha because it's more compatible with mongoose

---
+++ test/server.test.js
@@ -0,0 +1,12 @@
+var assert = require('assert');
+
+const app = require('../app')
+const supertest = require('supertest')
+const request = supertest(app)
+
+describe("/ endpoint", () => {
+    it("Should return a response 200", async () => {
+        const res = await request.get('/')
+        assert.equal(res.status, 200);
+    })
+})

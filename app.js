4ffca77f3a298f24f5a4314fccc51df835804571
Keclem <keclem8@aol.com>
2022-02-10 10:01:58 +0100

    Add morgan as logger

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
+const logger = require('morgan')
 const mountRoutes = require('./routes/index')
 
+app.use(logger('dev'))
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: true}))
 

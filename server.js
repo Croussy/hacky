71333b546cc67cf948002503a68c232e12bc95d5
Keclem <keclem8@aol.com>
2022-02-17 21:18:58 +0100

    Add default port for heroku 2

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const app = require('./server/app')
 const {dbConnect} = require('./server/config/db-manager')
 
-const port = process.env.PORT || 5000
+const port = 5000
 
 const server = app.listen(port, () => {
     console.log(`Listening on port ${port}`)

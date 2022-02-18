294e68f7cb655ce7534e88525fa20641573bb616
Keclem <keclem8@aol.com>
2022-02-18 10:13:58 +0100

    Add logo

--- client/src/component/game-controller/GameInformation.jsx
+++ client/src/component/game-controller/GameInformation.jsx
@@ -2,12 +2,11 @@ import React from 'react';
 import '../../styles/game-information.css'
 import GameInformationBlock from "./GameInformationBlock";
 
-const GameInformation = ({player, game}) => {
+const GameInformation = ({player}) => {
     return (
         <div className={'game-information'}>
-            <div className="logo">{game.name}</div>
+            <div className="logo"><img src={`${import.meta.env.VITE_REACT_APP_API_URL}/img/logo.svg`} alt={'logo hacky'}/></div>
             <GameInformationBlock label={'Score'} value={player.globalScore}/>
-            <GameInformationBlock label={'Temps'} value={'00:00:00'}/>
         </div>
     );
 };

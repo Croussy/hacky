32107548cc6f221de818b417b93e62a2428198ab
Keclem <keclem8@aol.com>
2022-02-20 21:44:04 +0100

    Add new favicon Fix bug on logo

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -10,7 +10,7 @@ import {Provider} from 'react-redux'
 
 const store = createStore(
     rootReducer,
-    composeWithDevTools(applyMiddleware(thunk))
+    applyMiddleware(thunk)
 )
 
 ReactDOM.render(
